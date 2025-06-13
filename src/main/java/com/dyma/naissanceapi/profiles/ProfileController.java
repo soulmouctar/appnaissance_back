6f7a3305b03177d0f1b6015d5197d822af862986
Souleymane Mouctar Diallo <soulmouctar96@gmail.com>
2025-06-13 23:31:23 +0000

    reading data from database

--- src/main/java/com/dyma/naissanceapi/profiles/ProfileController.java
+++ src/main/java/com/dyma/naissanceapi/profiles/ProfileController.java
@@ -5,10 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,10 +18,21 @@ public class ProfileController {
 
     private final ProfilesService profilesService;
 
-    @PostMapping("/create")
-    private void createProfile(@RequestBody Profile profile) {
+    @PostMapping(value = "create", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createProfile(@RequestBody Profile profile) {
         log.info("Creation du compte de : "+ profile.getEmail());
         this.profilesService.create(profile);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Profile> searchProfiles() {
+        return this.profilesService.search();
+    }
+
+    @GetMapping(path = "{id}")
+    public Profile read(@PathVariable int id) {
+        return this.profilesService.read(id);
+        //return null;
     }
 
 }
--- src/main/java/com/dyma/naissanceapi/profiles/ProfilesService.java
+++ src/main/java/com/dyma/naissanceapi/profiles/ProfilesService.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 @Slf4j
@@ -13,5 +16,15 @@ public class ProfilesService {
     public void  create(Profile profile) {
         log.info("Nouveau compte crée avec l'email :  {}",  profile.getEmail());
         profilesRespository.save(profile);
-    } 
+    }
+
+    public List<Profile> search() {
+        return this.profilesRespository.findAll();
+    }
+
+    public Profile read(int id) {
+        Optional<Profile> profile = this.profilesRespository.findById(id);
+        return profile.orElse(null);
+    }
+
 }
