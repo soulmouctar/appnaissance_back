1b30b30cc9a12c0c84fe7036e8e5373612096d51
Souleymane Mouctar Diallo <soulmouctar96@gmail.com>
2025-06-16 15:08:50 +0000

    specification des erreurs: time, type, status, message

--- src/main/java/com/dyma/naissanceapi/exceptions/ApplicationControllerAdvice.java
+++ src/main/java/com/dyma/naissanceapi/exceptions/ApplicationControllerAdvice.java
@@ -5,16 +5,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @ControllerAdvice
 public class ApplicationControllerAdvice {
-    
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
-    public ErrorEntity entityNotFoundException(EntityNotFoundException e) {
-        e.printStackTrace();
-        return new ErrorEntity(null, e.getMessage());
+    public @ResponseBody ErrorEntity entityNotFoundException(EntityNotFoundException e) {
+        //e.printStackTrace();
+        log.error("Erreur, {}", e.getMessage(), e) ;
+        return new ErrorEntity(
+                LocalDateTime.now(),
+                NOT_FOUND.value(),
+                null,
+                 e.getMessage());
     }
 }
--- src/main/java/com/dyma/naissanceapi/exceptions/ErrorEntity.java
+++ src/main/java/com/dyma/naissanceapi/exceptions/ErrorEntity.java
@@ -1,6 +1,10 @@
 package com.dyma.naissanceapi.exceptions;
 
+import java.time.LocalDateTime;
+
 public record ErrorEntity (
+        LocalDateTime time,
+        int status,
         String code,
         String message
 ){ }
