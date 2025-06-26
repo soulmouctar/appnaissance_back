08fcf7a407c7a734f0371a2c5a8ffe97feef0f18
Souleymane Mouctar Diallo <soulmouctar96@gmail.com>
2025-06-26 16:06:59 +0000

    refactoring

--- src/test/java/com/dyma/naissanceapi/profiles/ProfileRepositoryTest.java
+++ src/test/java/com/dyma/naissanceapi/profiles/ProfileRepositoryTest.java
@@ -2,6 +2,8 @@ package com.dyma.naissanceapi.profiles;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +11,23 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.jdbc.EmbeddedDatabaseConnection.H2;
 //Ajout des test pour le repository des profils
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = H2)
 public class ProfileRepositoryTest {
     @Autowired
+    ProfilesService profilesService;
+    @Autowired
     ProfilesRespository profilesRepository;
+
+    @Test
+    void setUp(){
+        Profile profileOne = Profile.builder().email("one@gmail.com").build();
+        Profile profileTwo = Profile.builder().email("two@gmail.com").build();
+        when(profilesService.search()).thenReturn(List.of(profileOne,profileTwo));
+    }
 
     @Test
     void shouldReturnListOfProfiles() {
@@ -30,5 +42,19 @@ public class ProfileRepositoryTest {
         // Assert
         Assertions.assertEquals(2, profiles.size());
 
+    }
+
+    @Test
+    void search() throws Exception {
+//        this.mockMVC
+    }
+
+    @Test
+    void create() throws JsonProcessingException {
+        //Arrange
+        Profile profileOne = Profile.builder().email("one@gmail.com").build();
+        String profileAsString = new ObjectMapper().writeValueAsString(profileOne);
+        //Act / Assert
+//        this.Moc
     }
 }
